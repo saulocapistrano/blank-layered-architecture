8ed7d24a46d61a8cbada45d93cc8e5f6cee9c2e8
saulocapistrano <saulocapistrano@gmail.com>
2025-10-07 13:53:59 -0300

    [feat](service): Implement BaseService and use domain exceptions

---
+++ src/main/java/com/blank/domain/service/BaseService.java
@@ -0,0 +1,29 @@
+package com.blank.domain.service;
+
+import com.blank.domain.exception.ResourceNotFoundException;
+import com.blank.domain.model.BaseEntity;
+import com.blank.domain.repository.BaseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BaseService {
+
+    private BaseRepository repository;
+
+    public BaseService(BaseRepository repository) {
+        this.repository = repository;
+    }
+
+    public BaseEntity create(BaseEntity entity){
+        if (entity.getName() == null || entity.getName().isEmpty()) {
+
+            throw new IllegalArgumentException("O nome não pode ser vazio.");
+        }
+        return repository.save(entity);
+    }
+
+    public BaseEntity findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Recurso não encontrado com Id: " + id));
+    }
+}
